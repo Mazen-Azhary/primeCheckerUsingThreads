746a377e44520a31022df10403377a61ac11b9db
Mazen Azhary <mazen.mazen9850@gmail.com>
2024-11-24 19:53:22 +0200

    wait and notify approach test , still not working

--- testthreading/NumberGenerator.java
+++ testthreading/NumberGenerator.java
@@ -3,49 +3,65 @@ package testthreading;
 import java.util.Scanner;
 
 public class NumberGenerator extends Thread {
+
     private int n;
-    private boolean sent;
+    private boolean generated;
+    private int a[];
 
     public NumberGenerator(int n) {
+        a = new int[1];
         this.setN(n);
-        sent=false;
+        generated = false;
         start();
     }
-
 
     public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = s.nextInt();
+        s.nextLine();
         NumberGenerator numberGenerator = new NumberGenerator(n);
         numberGenerator.join();
         s.close();
-        System.out.println("Time:"+(System.currentTimeMillis()-start)+" milliseconds");
+        System.out.println("Time:" + (System.currentTimeMillis() - start) + " milliseconds");
     }
 
-    
-    
+    public synchronized void run() {
 
-    public void run()  {
-        int[] numbers = new int[n];
+        PrimeChecker primeChecker = new PrimeChecker(this, a, n);
         for (int i = 0; i < n; i++) {
-            numbers[i] = i+1;
+            if (generated) {
+                try {
+                    System.out.println("in wait");
+                    wait();
+                } catch (InterruptedException ex) {
+                }
+            } else {
+                a[0] = i + 1;
+                System.out.println("generated num");
+                toggleGenerated();             
+                primeChecker.toggleGenerated();
+                notifyAll();
+            }
+
         }
-        PrimeChecker primeChecker = new PrimeChecker(numbers);
-        try{
-        primeChecker.join();
-        }catch(InterruptedException e){}
     }
 
     public void setN(int n) {
-        if (n >= 0)
+        if (n >= 0) {
             this.n = n;
-        else
+        } else {
             this.n = 10;
+        }
     }
 
     public int getN() {
         return n;
     }
+
+    public void toggleGenerated() {
+        this.generated = !this.generated;
+    }
+
 }
--- testthreading/PrimeChecker.java
+++ testthreading/PrimeChecker.java
@@ -1,30 +1,54 @@
 package testthreading;
 
-public class PrimeChecker extends Thread{
-    
+public class PrimeChecker extends Thread {
+
+    private int n;
+    private int[] a;
+    private boolean generated;
+    NumberGenerator parent;
+
     public boolean isPrime(int number) {
-        if (number <= 1)
+        if (number <= 1) {
             return false;
+        }
         for (int i = 2; i < number; i++) {
-            if (number % i == 0)
+            if (number % i == 0) {
                 return false;
+            }
         }
         return true;
     }
 
-    private int[] numbers;
-    public PrimeChecker(int[] numbers) {
-        this.numbers = numbers;
+    public PrimeChecker(NumberGenerator parent, int[] a, int n) {
+        this.n = n;
+        this.a = a;
+        this.parent = parent;
+        this.generated = true;
         start();
     }
 
-
-    public void run() {
-        for (int i = 0; i < numbers.length; i++) {
-            if (isPrime(numbers[i]))
-                System.out.println(numbers[i]);
+    public synchronized void run() {
+        for (int i = 0; i < n; i++) {
+            if (!generated) {
+                try {
+                    System.out.println("in checker wait");
+                    wait();
+                } catch (InterruptedException ex) {
+                }
+            } else {
+                System.out.println("in checker else");
+                toggleGenerated();
+                if (isPrime(a[0])) {
+                    System.out.println(a[0]);
+                }
+                parent.toggleGenerated();
+                notifyAll();
+            }
         }
     }
 
+    public void toggleGenerated() {
+        this.generated = !this.generated;
+    }
 
 }
